7003fb0affcd1ae8dca05513e069f4156c1f39d5
ZheerBarzan <zheerbarzan85@outlook.com>
2023-09-08 23:41:48 +0300

    add the navigaton to the windows application

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:window_manager/window_manager.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,11 +8,58 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WindowListener {
+  int index = 0;
+  final key = GlobalKey();
+
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+    return NavigationView(
+      key: key,
+      pane: NavigationPane(
+        items: [
+          PaneItem(
+            icon: const Icon(FluentIcons.news),
+            title: const Text('Top headlines'),
+            body: const Text("Top Headlines"),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.hot),
+            title: const Text('Top headlines'),
+            body: const Text("Top Headlines"),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.more_sports),
+            title: const Text('Top headlines'),
+            body: const Text("Top Headlines"),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.settings),
+            title: const Text('Top headlines'),
+            body: const Text("Top Headlines"),
+          ),
+        ],
+        selected: index,
+        onChanged: (value) {
+          setState(() {
+            index = value;
+          });
+          PaneDisplayMode.compact;
+        },
+      ),
+      content: null,
     );
   }
 }
