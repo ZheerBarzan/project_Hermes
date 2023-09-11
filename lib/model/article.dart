b8c806fb7b248e7ef297c6fb5456fdb25a49bdbe
ZheerBarzan <zheerbarzan85@outlook.com>
2023-09-11 18:03:51 +0300

    making connection between the application and the API

--- lib/model/article.dart
+++ lib/model/article.dart
@@ -1,1 +1,34 @@
-import 'package:fluent_ui/fluent_ui.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class Article {
+  final String title;
+  final String url;
+  final DateTime publishedAt;
+  final String source;
+  final String? urlToImage;
+
+  const Article({
+    required this.title,
+    required this.url,
+    required this.publishedAt,
+    required this.source,
+    required this.urlToImage,
+  });
+
+  String captionText() {
+    final formattedPublishedAt =
+        timeago.format(publishedAt, locale: "en_short");
+
+    return "$source $formattedPublishedAt";
+  }
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? "",
+      url: json['url'] ?? "",
+      publishedAt: DateTime.tryParse(json['publishedAt']) ?? DateTime.now(),
+      source: json['source']['name'] ?? "",
+      urlToImage: json['urlToImage'] ?? "",
+    );
+  }
+}
---
+++ lib/services/news_api.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart%20';
+import 'package:project_hermes/model/article.dart';
+import 'package:project_hermes/model/article_catagory.dart';
+
+class NewsApi {
+  const NewsApi();
+
+  static const baseUrl = "https://newsapi.org/v2";
+  static const apiKey = "c212bebdac9741d3870383d4ca2d4e1f";
+
+  Future<List<Article>> fetchArticles(ArticleCatagory catagory) async {
+    var url = NewsApi.baseUrl;
+
+    url += "/top-headlines";
+    url += "?apiKey=$apiKey";
+    url += "&language=en";
+    url += "&catagory=${queryParams(catagory)}";
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      if (json['status'] == "ok") {
+        final dynamic articleJSON = json['articles'] ?? [];
+        final List<Article> articles = articleJSON.map<Article>((e) {
+          Article.fromJson(e);
+        });
+        return articles;
+      } else {
+        throw Exception(json['messege'] ?? 'Failed to load ');
+      }
+    } else {
+      throw Exception("bad respose");
+    }
+  }
+
+  String queryParams(ArticleCatagory catagory) {
+    switch (catagory) {
+      case ArticleCatagory.general:
+        return "general";
+      case ArticleCatagory.business:
+        return "business";
+      case ArticleCatagory.technology:
+        return "technology";
+      case ArticleCatagory.entertainment:
+        return "entertainment";
+      case ArticleCatagory.sports:
+        return "sports";
+      case ArticleCatagory.science:
+        return "science";
+      case ArticleCatagory.health:
+        return "health";
+
+      default:
+        return "general";
+    }
+  }
+}
